1cffec49212937c317c1e6217fa85b2778d50455
Daniel Szaniszlo <daniel.szaniszlo@ericsson.com>
2017-03-30 13:15:22 +0200

    Error handling

--- routes/content.js
+++ routes/content.js
@@ -23,26 +23,35 @@ var contentManager = function(app, Content) {
     app.post('/api/content-store/add', function(req, res) {
         // insert into/overwrite existing application in mongodb document
         var applicationName = req.body.application;
-        var content = req.body.content;
+        var content = req.body.content; // the posted content should be a stringified object
 
-        Content.findOneAndUpdate({
-            "application": applicationName
-        }, {
-            "content": JSON.parse(content) //escaped quotes on keys, escaped quotes on string values
-        }, {
-            new: true,
-            upsert: true
-        }, function(err, doc) {
-            if (err) {
-                res.status(400).json({
-                    result: err
-                });
-            } else {
-                res.status(200).json({
-                    msg: "New settings: " + doc
-                });
-            }
-        });
+        try {
+
+            content = JSON.parse(content);
+            Content.findOneAndUpdate({
+                "application": applicationName
+            }, {
+                "content": content
+            }, {
+                new: true,
+                upsert: true
+            }, function(err, doc) {
+                if (err) {
+                    res.status(400).json({
+                        result: err
+                    });
+                } else {
+                    res.status(200).json({
+                        msg: "New settings: " + doc
+                    });
+                }
+            });
+        } catch (e) {
+            res.status(500).json({
+                msg: "Something went wrong: " + e
+            });
+        }
+
     });
 };
 
